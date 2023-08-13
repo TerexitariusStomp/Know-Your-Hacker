8b86361a27222ee26cc32f36350c8291122dc4e1
FrankBevr <frank_dierolf@web.de>
2023-08-13 06:35:14 +0200

    :zap: (./Fronto) reshuffle Header

--- Fronto/src/components/Header.vue
+++ Fronto/src/components/Header.vue
@@ -1,3 +1,18 @@
 <template>
-  <div></div>
+  <div>
+    <!---------->
+    <!--Header-->
+    <!---------->
+    <div class="site-header">
+      <div class="site-le">
+        <span class="site-text"><span>Know Your Hacker</span></span>
+      </div>
+      <div class="site-ce">
+        <img src="public/external/aaveeth1318-rh5l-200h.png" alt="aaveeth1318" class="site-aaveeth" />
+      </div>
+      <div class="site-re2">
+        <span class="site-text02"><span>Connect wallet</span></span>
+      </div>
+    </div>
+  </div>
 </template>
